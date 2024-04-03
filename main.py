9b0e58bcc35781e39f2720931fcbb0bae94c1ca8
Arnav Nagpurkar <arnavn.dev@gmail.com>
2024-04-03 22:11:01 +0530

    Now Compatible on All PC OS

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# Works only on windows
-
 # Installing win10toast on your or some person's computer
-import os
-os.system("pip install win10toast") # Uncomment these three lines if win10toast module is not installed, if installed comment it out
 
-from win10toast import ToastNotifier
+from notifypy import Notify
 import datetime
 import time
 
 
 # Create a toast notifier object
-toaster = ToastNotifier()
+notification = Notify()
 
 # Name
 name = input("Enter your name: ")
@@ -19,30 +15,16 @@ name = name.title()
 # After how many minutes to display the notification?
 notificationTime = int(input(f"In how many min(s) would you like me to remind you to drink water? "))
 
-# # How many times to display the notification? (Uncomment for using for loop and comment for using while loop)
-# howMany = int(input("How many times would I remind you to drink water? "))
-
-
-# # Final Program to Loop
-
-# for i in range(howMany):
-#     # Loading the current timeCurrent time
-#     currentTime = datetime.datetime.now()
-#     timeNow = currentTime.strftime("%H:%M:%S")
-#
-#     # Displaying the notification
-#     toaster.show_toast(f"Drink Water", f"Drink Water {name} its {timeNow}!", duration=10)
-#     # Waiting for the given time and again displaying notification
-#     time.sleep(notificationTime * 60)
-# --------------------------OR---------------------------------
-
 while True:
     # Loading the current timeCurrent time
     currentTime = datetime.datetime.now()
     timeNow = currentTime.strftime("%H:%M:%S")
 
     # Displaying the notification
-    toaster.show_toast(f"Drink Water", f"Drink Water {name} its {timeNow}!", duration=10)
+    notification.title = f"Drink Water {name}!!"
+    notification.message = f"Drink Water it's {timeNow} you drank water {notificationTime} mins ago !!"
+    notification.send()
+
     # Waiting for the given time and again displaying notification
     time.sleep(notificationTime * 60)
 
